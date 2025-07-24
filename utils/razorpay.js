64f4c276daabe6c39183796c241226d9d062175f
saurabh eknath dokade <saurabhdokade77@gmail.com>
2025-07-24 18:01:41 +0530

    update a code

--- utils/razorpay.js
+++ utils/razorpay.js
@@ -1,8 +1,8 @@
 const Razorpay = require("razorpay");
 
 const instance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID || "rzp_live_D3D9CzhhPmwAZe",
-  key_secret: process.env.RAZORPAY_KEY_SECRET || "gTPUidHTVpnljtGjLZHUcFV4",
+  key_id: process.env.RAZORPAY_KEY_ID || "rzp_live_qdIou8vehX7Xmn",
+  key_secret: process.env.RAZORPAY_KEY_SECRET || "r19BSEtKPTGxb3mQsfatNA2S",
 });
 
 module.exports = instance;
